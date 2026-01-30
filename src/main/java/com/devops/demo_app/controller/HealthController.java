4994f2684daf082b494cc2e1192998d42b819ef8
Harish <pobbathiharish.hp@gmail.com>
2026-01-30 22:38:14 +0530

    Initial Spring Boot app with health endpoint

---
+++ src/main/java/com/devops/demo_app/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.devops.demo_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "DevOps Application is Running";
+    }
+}
