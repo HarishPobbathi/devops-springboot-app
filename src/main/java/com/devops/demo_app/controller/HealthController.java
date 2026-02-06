d87c412ca9629abf9690d774fec7ddbcaca0d632
Harish <pobbathiharish.hp@gmail.com>
2026-02-06 19:51:10 +0530

    Verify Jenkins auto deploy with new health message

--- src/main/java/com/devops/demo_app/controller/HealthController.java
+++ src/main/java/com/devops/demo_app/controller/HealthController.java
@@ -8,7 +8,7 @@ public class HealthController {
 
     @GetMapping("/health")
     public String health() {
-        return "CI/CD Pipeline Working 🚀";
+        return "Auto Deploy Verified via Jenkins";
 
     }
 }
